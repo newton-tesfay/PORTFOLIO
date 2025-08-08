fe2f64e9e4911e618d36c9b97a1f403ad50ff8ef
Newton <newtontesfay23@gmail.com>
2025-08-09 00:58:17 +0300

    updates

---
+++ scripts.js
@@ -0,0 +1,19 @@
+// smooth scrolling for anchor tags (links)
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+    anchor.addEventListener("click", function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute("href")).scrollIntoView({
+            behavior: "smooth",
+        })
+    })
+})
+
+// for submit handling
+
+document
+.getElementById("contact")
+.addEventListener("submit", function (e){
+    e.preventDefault()
+    alert("Thanks For Your Message"):
+    this.requestFullscreen();
+});
